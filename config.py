85f90eb345b6d719e36677c8364843b1d4d57edb
vladgabrielboca <vladgabrielboca@gmail.com>
2026-05-07 15:53:52 +0300

    updating context length

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Data parameters
-CONTEXT_LENGTH = 72
+CONTEXT_LENGTH = 96
 HORIZON = 18
 MAX_SERIES = 5000  # None for all available series
 
@@ -18,4 +18,4 @@ LEARNING_RATE = 1e-3
 
 # File paths
 TRAIN_CSV_PATH = "./data/Monthly-train.csv"
-MODEL_SAVE_PATH = "transformer_ctx72_best.keras"
+MODEL_SAVE_PATH = "transformer_ctx96_best.keras"
